80a16c4dfec00a45303f6ad7ba068f299293ea8e
Romaric Thibault <torburma@gmail.com>
2020-02-11 20:59:06 +0100

    Let's rework the OVH DynDNS

--- OVH_DynDNS_v2/Program.cs
+++ OVH_DynDNS_v2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace OVH_DynDNS_v2
 {
@@ -6,7 +8,27 @@ namespace OVH_DynDNS_v2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+
+            IConfigurationRoot configuration = builder.Build();
+
+            Config config = new Config();
+            configuration.Bind(config);
+
+            Console.WriteLine(config.OvhApplicationKey);
+            Console.WriteLine(config.OvhApplicationSecret);
+            Console.WriteLine(config.OvhConsumerKey);
+            Console.WriteLine(config.OvhDomainName);
         }
+    }
+
+    public class Config
+    {
+        public string OvhApplicationKey { get; set; }
+        public string OvhApplicationSecret { get; set; }
+        public string OvhConsumerKey { get; set; }
+        public string OvhDomainName { get; set; }
     }
 }
